b581130ef8f7eedc67cfa66c3b43e8c9143be93e
ThinkInFuture <125987347@qq.com>
2026-09-12 09:50:20 +0800

    share: widen touch detection, bump cache v2

--- share.js
+++ share.js
@@ -22,7 +22,8 @@ sysItem.className='hs-share-item';
 sysItem.innerHTML='<svg viewBox="0 0 24 24"><path d="M18 16.1c-.8 0-1.5.3-2 .8l-7.1-4.2c0-.2.1-.5.1-.7s0-.5-.1-.7L16 7.2c.5.5 1.2.8 2 .8 1.7 0 3-1.3 3-3s-1.3-3-3-3-3 1.3-3 3c0 .2 0 .5.1.7L8 9.8C7.5 9.3 6.8 9 6 9c-1.7 0-3 1.3-3 3s1.3 3 3 3c.8 0 1.5-.3 2-.8l7.1 4.2c0 .2-.1.4-.1.6 0 1.6 1.3 2.9 2.9 2.9s2.9-1.3 2.9-2.9-1.2-2.9-2.8-2.9z"/></svg><span>更多分享</span>';
 panel.appendChild(copyItem);
 panel.appendChild(wxItem);
-if(navigator.share&&window.matchMedia&&matchMedia('(pointer: coarse)').matches){
+var isTouch=(window.matchMedia&&matchMedia('(pointer: coarse)').matches)||(navigator.maxTouchPoints>0);
+if(navigator.share&&isTouch){
 panel.appendChild(sysItem);
 sysItem.addEventListener('click',function(){
 panel.classList.remove('open');
